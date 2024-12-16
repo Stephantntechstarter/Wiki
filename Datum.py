573955b46901abfff6a789b51112444bd38fa558
Stephan <stephan.schulz@tn.techstarter.de>
2024-12-16 02:05:38 +0100

    Aufgabe

---
+++ Datum.py
@@ -0,0 +1,17 @@
+from datetimw import datetime
+
+datum_input = input("Datum (YYYY.MM.DD):")
+
+try:
+    datum =datetimw.strptime(datum_input, "%Y.%m.%d")
+
+    weihnachtsferien_start = datetime(2024, 12, 24)
+    weihnachtsferien_ende = datetime(2025, 1, 1)
+
+    if weihnachtsferien_start <= datum <= weihnachtsferien_ende:
+        print("Weihnachtsferien!")
+    else:
+        print("Kurszeit!")
+
+except ValueError:
+    print("Ungültiges Datum.")
